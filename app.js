56500cdcf970ca1cf28854b39e7bdd752a5f2440
Priyanshu Patel <139453653+code-withpriyanshu@users.noreply.github.com>
2024-04-23 15:06:35 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let bg = document.querySelector(".container");
+
+
+function bgcolourblue(){ 
+    bg.setAttribute("class" , "blue")
+}
+function bgcolourred(){ 
+    bg.setAttribute("class" , "red")
+
+}
+
+for(let i = 0 ; i < 9 ; i++){
+    
+}
